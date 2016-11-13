9effd8ac98a66f2f01531425d026de18040ad88c
Sagar Dutta <sagar_dutta@intuit.com>
2016-11-13 13:16:12 -0600

    2. middleware and routes explained

--- sample-express/app.js
+++ sample-express/app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express()
 
+app.use(function(req,res, next){
+  console.log(req.path);
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.post('/metrics', function (req, res) {
+  res.send('Metrics what ??')
 })
 
 
